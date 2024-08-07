c300b8e957a33daff8e6306c7613ac97ca436bcb
Caíque Cardoso <caiquecardoso.ti@gmail.com>
2024-08-07 20:30:15 -0300

    Adição de comentário no model Agenda

--- app/Models/Agenda.php
+++ app/Models/Agenda.php
@@ -9,6 +9,11 @@ class Agenda extends Model
 {
     use HasFactory;
     
+    /**
+     * determina os atributos dessa mondel, necessário para edição via ORM
+     *
+     * @var array
+     */
     protected $fillable = [
         'servidor_id',
         'aluno_id',
@@ -17,21 +22,45 @@ class Agenda extends Model
         'status'
     ];
     
+    /**
+     * determina o relacionamento entre Agenda e Aluno
+     * Aqui é dito o grau de relacionamento 
+     * belongsTo = pertence. Uma AGENDA (horário, agendamento...) pertence a um Aluno
+     * Relação de 1 : 1
+     * @return void
+     */
     public function aluno()
     {
         return $this->belongsTo(Aluno::class, 'aluno_id');
     }
     
+    /**
+     * determina o relacionamento entre Agenda e Servidor
+     * Aqui é dito o grau de relacionamento 
+     * belongsTo = pertence. Uma AGENDA (horário, agendamento...) pertence a um Servidor
+     * Relação de 1 : 1
+     * @return void
+     */
     public function servidor()
     {
         return $this->belongsTo(Servidor::class, 'servidor_id');
     }
     
+    /**
+     * define a forma como um determinado atributo vai ser retornado ao ser consultado
+     *
+     * @return void
+     */
     public function getDataAttribute()
     {
         return date('d/m', strtotime($this->attributes['data']));
     }
     
+    /**
+     * define a forma como um determinado atributo vai ser retornado ao ser consultado
+     *
+     * @return void
+     */
     public function getHorarioAttribute()
     {
         return date('H:i', strtotime($this->attributes['horario']));
